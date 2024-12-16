0a44917593efa5cd33d703f05b6c312467b17051
Alexander Anova H <arthurlion345@gmail.com>
2024-12-16 20:14:44 +0700

    Delete TubesRPL directory

--- TubesRPL/src/main/java/com/example/TubesRPL/repository/generalRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.TubesRPL.repository;
-
-import com.example.TubesRPL.data.loginData;
-
-public interface generalRepo {
-
-    loginData login(String nohp, String pass);
-}
--- TubesRPL/src/main/resources/static/dokter/diagnosa/diagnosaScript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('umur').addEventListener('input', function (e) {        
-    this.value = this.value.replace(/[^0-9]/g, '');
-});
-
